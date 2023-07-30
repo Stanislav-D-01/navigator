a5cb49597268a7b8b149c59d4aed93aefb0f8693
Stas Dubinin <dubinin.stas@gmail.com>
2023-07-30 16:35:52 +0400

    good

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 export const requestRoutes = (coord) => {
   return fetch(
-    `http://router.project-osrm.org/route/v1/driving/${coord[0][1]},${coord[0][0]};${coord[1][1]},${coord[1][0]};${coord[2][1]},${coord[2][0]}?geometries=geojson&continue_straight=false&annotations=true&overview=full`,
+    `https://router.project-osrm.org/route/v1/driving/${coord[0][1]},${coord[0][0]};${coord[1][1]},${coord[1][0]};${coord[2][1]},${coord[2][0]}?geometries=geojson&overview=full`,
     {
       method: "GET",
     }
